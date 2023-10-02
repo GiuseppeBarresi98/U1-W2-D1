6650c53a9658ff8efa4aae8cebe80878e2cbe0fd
Giuseppe Barresi <peppe.barresi1@gmail.com>
2023-10-02 18:02:25 +0200

    2nd commit

--- D1.js
+++ D1.js
@@ -63,4 +63,4 @@ let name1 = "john";
 let name2 = "John";
 
 console.log(name1 === name2);
-console.log(name1.toLowerCase === name2.toLowerCase);
+console.log(name1.toLowerCase() === name2.toLowerCase());
